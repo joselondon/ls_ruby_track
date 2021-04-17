97f75acece8fe6ddbbfd1002917b30aad71ff1d6
joselondon <copovikingj@gmail.com>
2021-04-17 19:15:24 +0100

    Adds a working basic car_calculator.rb program

--- rb101/rb101_lesson_2/car_loan_calculator.rb
+++ rb101/rb101_lesson_2/car_loan_calculator.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #principle = 10000
 #apr = 0.18
 #annual_duration = 5
@@ -49,6 +50,32 @@
 
 # END
 
+another_loan = 'y'
+  Kernel.puts("Welcome to the loan calculator.")
 
+while another_loan == 'y'
+  Kernel.puts("Please enter the loan amount:")
+  principle = Kernel.gets().chomp()
+  
+  Kernel.puts("Thank you, now enter the annual interest rate on the loan:")
+  apr = Kernel.gets().chomp().to_f / 100
 
+  Kernel.puts("Now please enter the loan duration in years:")
+  duration_years = Kernel.gets().chomp()
+
+  Kernel.puts("Thank you, calculating you loan details...")
+
+  duration_months = duration_years.to_i * 12
+  monthly_int = (apr / 12)
+  monthly_payment = principle.to_i * (monthly_int / (1 - (1 + monthly_int)**(-duration_months)))
+
+  Kernel.puts("Your monthy payment will be " + monthly_payment.to_s)
+  Kernel.puts("Your loan will repayments will continue for " + duration_months.to_s + " months")
+  Kernel.puts("your monthly interest rate will be " + monthly_int.to_s + "%")
+
+  Kernel.puts("Do you want to check another loan? ('y' for yes)")
+  another_loan = Kernel.gets().chomp()
+end
+
+Kernel.puts("Thank you for using the loan calculator.  Goodbye!")
 
