8e9f70a6b9f2dbcca05c4a9b821011606520936b
joselondon <copovikingj@gmail.com>
2021-04-18 10:15:43 +0100

    Adds a better prompt for console messages.   Adds rounding for floats.

--- rb101/rb101_lesson_2/car_loan_calculator.rb
+++ rb101/rb101_lesson_2/car_loan_calculator.rb
@@ -1,19 +1,4 @@
 require 'pry'
-#principle = 10000
-#apr = 0.18
-#annual_duration = 5
-#duration_months = annual_duration * 12
-#monthly_int = apr / 12
-#
-#
-#
-#
-#monthly_payment = principle * (monthly_int / (1 - (1 + monthly_int)**(-duration_months)))
-#
-#p monthly_payment
-#p duration_months
-#p monthly_int
-#--------------------------------------------------------------------------------------------
 
 # PSEUDO CODE
 # given the loan amount, APR, and and loan duration in years
@@ -50,32 +35,36 @@ require 'pry'
 
 # END
 
+def prompt(message)
+  Kernel.puts(">> #{message}")
+end
+
 another_loan = 'y'
-  Kernel.puts("Welcome to the loan calculator.")
+  prompt("Welcome to the loan calculator.")
 
 while another_loan == 'y'
-  Kernel.puts("Please enter the loan amount:")
+  prompt("Please enter the loan amount:")
   principle = Kernel.gets().chomp()
   
-  Kernel.puts("Thank you, now enter the annual interest rate on the loan:")
+  prompt("Thank you, now enter the annual interest rate on the loan:")
   apr = Kernel.gets().chomp().to_f / 100
 
-  Kernel.puts("Now please enter the loan duration in years:")
+  prompt("Now please enter the loan duration in years:")
   duration_years = Kernel.gets().chomp()
 
-  Kernel.puts("Thank you, calculating you loan details...")
+  prompt("Thank you, calculating you loan details...")
 
   duration_months = duration_years.to_i * 12
   monthly_int = (apr / 12)
   monthly_payment = principle.to_i * (monthly_int / (1 - (1 + monthly_int)**(-duration_months)))
 
-  Kernel.puts("Your monthy payment will be " + monthly_payment.to_s)
-  Kernel.puts("Your loan will repayments will continue for " + duration_months.to_s + " months")
-  Kernel.puts("your monthly interest rate will be " + monthly_int.to_s + "%")
+  prompt("Your monthy payment will be: £" + monthly_payment.round(2).to_s)
+  prompt("Your loan will repayments will continue for " + duration_months.to_s + " months")
+  prompt("your monthly interest rate will be " + monthly_int.round(4).to_s + "%")
 
-  Kernel.puts("Do you want to check another loan? ('y' for yes)")
+  prompt("Do you want to check another loan? ('y' for yes)")
   another_loan = Kernel.gets().chomp()
 end
 
-Kernel.puts("Thank you for using the loan calculator.  Goodbye!")
+prompt("Thank you for using the loan calculator.  Goodbye!")
 
