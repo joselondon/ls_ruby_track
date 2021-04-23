e3814b1615aef3ec12212937ce9c87c71c09487e
joselondon <copovikingj@gmail.com>
2021-04-23 08:55:05 +0100

    Halfway through rock, paper, scissors walkthrough

---
+++ rb101/rb101_lesson_2/wt_rock_paper_scissors.rb
@@ -0,0 +1,27 @@
+VALID_CHOICES = ['rock', 'paper', 'scissors']
+
+def prompt(message)
+  Kernel.puts(">> #{message}")
+end
+
+choice = ''
+loop do
+  prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+  choice = Kernel.gets().chomp()
+
+  if VALID_CHOICES.include?(choice)
+    break
+  else
+    prompt("That's not a valid choice.")
+  end
+end
+
+computer_choice = VALID_CHOICES.sample()
+
+prompt("You chose: #{choice}; Computer chose: #{computer_choice}")
+
+if (choice == 'rock' && computer_choice == 'scissors') ||
+    (choice == 'paper' && computer_choice == 'rock') ||
+    (choice == 'scissors' && computer_choice == 'paper')
+  prompt("You won!")
+elsif ()
