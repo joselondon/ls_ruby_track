5fedf02dd29221077cecfdde8f94cc9a7be294c1
joselondon <copovikingj@gmail.com>
2021-05-07 18:22:01 +0100

    Refactoring car_loan_calculator.rb:  Adds timer functionality, tidies up UI, adds quit input validation

--- rb101/rb101_lesson_2/exercises/car_loan_calculator.rb
+++ rb101/rb101_lesson_2/exercises/car_loan_calculator.rb
@@ -2,6 +2,8 @@ require 'yaml'
 MESSAGES = YAML.load_file('car_loan_calculator_messages.yml')
 LANGUAGE = 'en'
 REGEX_STRING_REAL_NUMS_GREATER_THAN_ZERO = /\A[1-9]\d*$\Z/
+VALID_YES = ['y', 'yes']
+VALID_NO = ['n', 'no']
 
 def messages(message, lang='en')
   MESSAGES[lang][message]
@@ -20,10 +22,33 @@ def whole_years_input_validation(num)
   !!REGEX_STRING_REAL_NUMS_GREATER_THAN_ZERO.match(num)
 end
 
-another_loan = 'y'
+def clear_console
+  system('clear')
+  system('cls')
+end
+
+def timer
+sleep(1)
+  puts('-')
+  sleep(1)
+  puts('-')
+  sleep(1)
+  puts('-')
+end
+
+def valid_quit_options(input)
+  if VALID_YES.include?(input) || VALID_NO.include?(input)
+    true
+  else
+    false
+  end
+end
+
 prompt('welcome')
 
-while another_loan == 'y'
+loop do
+  timer()
+  clear_console
   principle = nil
   prompt('principle')
   loop do
@@ -61,6 +86,8 @@ while another_loan == 'y'
   end
 
   prompt('calculating')
+  
+  timer()
 
   duration_months = duration_years.to_i * 12
   monthly_int = (apr_decimal / 12)
@@ -72,8 +99,18 @@ while another_loan == 'y'
   prompt('months', duration_months, " months")
   prompt('monthly_int', monthly_percentage.round(4), "%")
 
+  another_loan = ''
   prompt('again?')
-  another_loan = Kernel.gets().downcase().chomp()
+  loop do
+    another_loan = Kernel.gets().downcase().chomp()
+    if valid_quit_options(another_loan) == false
+      prompt('valid_quit')
+    else
+      break
+    end
+  end
+
+  VALID_NO.include?(another_loan) ? break : next
 end
 
 prompt('goodbye')
