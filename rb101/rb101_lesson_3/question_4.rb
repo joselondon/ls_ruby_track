371a8e13218a3c331418c2b174e6bb4ede9a82a1
joselondon <copovikingj@gmail.com>
2021-05-23 15:04:41 +0100

    Question_4.rb

---
+++ rb101/rb101_lesson_3/question_4.rb
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5]
+
+# both methods are destructive: -
+
+numbers.delete_at(1) # deletes the argument at the index passed as an argument in this case 1.
+                     # numbers = [1, 3, 4, 5].  Deleted objectis returned
+numbers.delete(1) # deletes objects matching the argument passed to the method ie 1.  
+                  # numbers = [2, 3, 4, 5]   The last object deleted is returned ie 1.
