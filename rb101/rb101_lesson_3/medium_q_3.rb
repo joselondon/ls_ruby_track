f2c52e7394243e1eedadc6dc4fe307c9f021d498
joselondon <copovikingj@gmail.com>
2021-05-25 18:12:49 +0100

    Starts question medium q 3

---
+++ rb101/rb101_lesson_3/medium_q_3.rb
@@ -0,0 +1,13 @@
+def factors(number)
+  divisor = number
+  factors = []
+  begin
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end until divisor == 0
+  factors
+end
+
+p factors(12)
+p factors(0)
+
