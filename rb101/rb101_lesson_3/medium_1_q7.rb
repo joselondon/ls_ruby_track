fad784dc12673f311ec8fdc7d99e53f09e82cb4d
joselondon <copovikingj@gmail.com>
2021-05-26 18:12:26 +0100

    Solution to medium #1 question 7

---
+++ rb101/rb101_lesson_3/medium_1_q7.rb
@@ -0,0 +1,21 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+# The hash is indeed altered (both the age and gender values)
+# The musters hash object is being assigned to the mess_with_demographics() demo_hash 
+# parameter.   #values works directly on that object pointing the 
+# 'age' and 'gender' elements to new values/objects in place.
