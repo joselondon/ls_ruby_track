acf86e81d1cf1dcc59d70d85ee2c7b3514fd525e
joselondon <copovikingj@gmail.com>
2021-05-27 18:11:09 +0100

    medium_1_q8

---
+++ rb101/rb101_lesson_3/medium_1_q8.rb
@@ -0,0 +1,18 @@
+def rps(fist1, fist2)
+  if fist1 == "rock"
+    (fist2 == "paper") ? "paper" : "rock"
+  elsif fist1 == "paper"
+    (fist2 == "scissors") ? "scissors" : "paper"
+  else
+    (fist2 == "rock") ? "rock" : "scissors"
+  end
+end
+
+puts rps(rps(rps("rock", "paper"), rps("rock", "scissors")), "rock")
+
+
+# paper
+
+# The outer parentheses evaluate to rock
+# The middle parentheses evaluate to paper and rock which equals paper which beats the outer parantheses of paper
+
