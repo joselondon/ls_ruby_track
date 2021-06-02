4d66e58a06ee15f803cd89209f13bc5940ef5604
joselondon <copovikingj@gmail.com>
2021-06-02 18:22:50 +0100

    Adds solution for hard question 3

--- rb101/rb101_lesson_3/hard_q3.rb
+++ rb101/rb101_lesson_3/hard_q3.rb
@@ -43,7 +43,7 @@ puts "three is: #{three}" # "three"
 
 # C)
 def mess_with_vars(one, two, three)
-  one.gsub!("one","two")
+  one.gsub!("one","two")    
   two.gsub!("two","three")
   three.gsub!("three","one")
 end
@@ -54,6 +54,6 @@ three = "three"
 
 mess_with_vars(one, two, three)
 
-puts "one is: #{one}"
-puts "two is: #{two}"
-puts "three is: #{three}"
+puts "one is: #{one}"     # "two"
+puts "two is: #{two}"     # "three"
+puts "three is: #{three}" # "one"
