b335ef78308ef9670477cc077ed778f314423026
joselondon <copovikingj@gmail.com>
2021-06-03 08:03:04 +0100

    hard question 4 - now correctly spits out tru or false for each of the numbers in the ip address

--- rb101/rb101_lesson_3/hard_q4.rb
+++ rb101/rb101_lesson_3/hard_q4.rb
@@ -1,16 +1,18 @@
 def is_an_ip_number?(str)
-  (1..255) === str.to_i
+  (1..255) === str
 end
 
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
   while dot_separated_words.size > 0 do
-    word = dot_separated_words.pop
-    break unless is_an_ip_number?(word)
+    word = dot_separated_words.pop.to_i
+    if is_an_ip_number?(word) == false
+      return false
+    end
   end
   return true
 end
 
 
 
-p dot_separated_ip_address?("10000.1.500.100sdasd0")
+p dot_separated_ip_address?("10.254.1.1")
