e708b598ddcba9541df6fd7fbbd8bb0f604fb76f
joselondon <copovikingj@gmail.com>
2021-06-03 08:22:06 +0100

    hard question 4 - adds check on 4 parts only in IP number

--- rb101/rb101_lesson_3/hard_q4.rb
+++ rb101/rb101_lesson_3/hard_q4.rb
@@ -1,18 +1,30 @@
-def is_an_ip_number?(str)
-  (1..255) === str
-end
 
+# my solution
 def dot_separated_ip_address?(input_string)
   dot_separated_words = input_string.split(".")
+  return false if (array_equals_4?(dot_separated_words) == false)
+
   while dot_separated_words.size > 0 do
     word = dot_separated_words.pop.to_i
     if is_an_ip_number?(word) == false
       return false
     end
   end
-  return true
+  
+  true
 end
 
-
-
 p dot_separated_ip_address?("10.254.1.1")
+
+# more elegant!
+def dot_separated_ip_address?(input_string)
+  dot_separated_words = input_string.split(".")
+  return false unless dot_separated_words.size == 4
+
+  while dot_separated_words.size > 0 do
+    word = dot_separated_words.pop
+    return false unless is_an_ip_number?(word)
+  end
+
+  true
+end
