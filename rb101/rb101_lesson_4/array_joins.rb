82572271146841b1537fee06ac9d4f5e9ddd204c
joselondon <copovikingj@gmail.com>
2021-06-04 08:55:06 +0100

    Adds some lesson workings

---
+++ rb101/rb101_lesson_4/array_joins.rb
@@ -0,0 +1,5 @@
+str = 'How do you get to Carnegie Hall?'
+arr = str.split # => ["How", "do", "you", "get", "to", "Carnegie", "Hall?"]
+arr.join        # => "HowdoyougettoCarnegieHall?"
+
+arr.join(' ')   # =>["How", "do", "you", "get", "to", "Carnegie", "Hall?"]
