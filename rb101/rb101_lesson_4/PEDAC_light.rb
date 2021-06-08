96ee162517ceef8274094637538a116d09468fe3
joselondon <copovikingj@gmail.com>
2021-06-08 08:09:28 +0100

    Final type up of PEDAC light to solidify understanding

--- rb101/rb101_lesson_4/PEDAC_light.rb
+++ rb101/rb101_lesson_4/PEDAC_light.rb
@@ -91,10 +91,10 @@ end
 
 # - create an empty array called 'result' that willcontain all required substrings
 # - create a 'starting_index' variable (value '0') for the starting index of a substring
-# - start a loop tha iterates over 'starting_index' from '0' to th elength of the string minus 2
+# - start a loop tha iterates over 'starting_index' from '0' to the length of the string minus 2
 #   - create a 'num_chars' variable (value '2')  for the length of a substring
 #   - start an inner loop that iterates over 'num_chars' from '2' to 'string.length -starting_index'
-#     - extract asubstring of length 'num_chars' from 'string' starting at 'starting_index'
+#     - extract a substring of length 'num_chars' from 'string' starting at 'starting_index'
 #     - append the extracted substring to the 'result' array
 #     - increment the 'num_chars' variable by '1'
 #   - end the inner loop
@@ -106,7 +106,121 @@ end
 # Optional step could be to turn this into psuedocode: 
 # ============================================================================================
 
+# START
+#
+#   /* Given a string named 'string' * /
+#   
+#   SET result = []
+#   SET starting_index = 0
+#
+#   WHILE starting_index <= length of string - 2
+#     SET num_Chars = 2
+#     WHILE num_Chars <= length of string - starting_index
+#       SET substring = num_chars characters from string starting at index starting_index
+#       append substring to result array
+#       SET num_chars = num_chars + 1
+#
+#     SET starting_index = starting_index +1
+#
+#   RETURN result
+#
+#END
 
+def substring(str)
+  result = []
+  starting_index = 0;
 
+  while (starting_index <= str.length -2_
+    num_chars = 2
+    while (num_chars <= str.length - starting_index)
+      substring = str.slice(starting_index, num_chars)
+      result << substring
+      num_chars += 1
+    end
+    starting_index += 1
+  end
+  result
+end
 
+# Palindrome check algorithm
+
+# - Inside the 'is_palindrome?' method, check whther the string
+#   value is equal to its reversed value.  You can use the
+#   String#reverse method.
+
+def is_palidrom?(str)
+  return str == str.reverse
+end
+
+# FULL FINAL ALGORITHM
+
+# input:  a string
+# output: and array of substrings
+# rules:  palindrome words shouldbe cse sensitive, meaning "sbBA"
+#         is not a palindrome
+
+# Algorithm
+# substrings method
+# =================
+# - create an empty array called 'result' that willcontain all required substrings
+# - create a 'starting_index' variable (value '0') for the starting index of a substring
+# - start a loop tha iterates over 'starting_index' from '0' to the length of the string minus 2
+#   - create a 'num_chars' variable (value '2')  for the length of a substring
+#   - start an inner loop that iterates over 'num_chars' from '2' to 'string.length -starting_index'
+#     - extract a substring of length 'num_chars' from 'string' starting at 'starting_index'
+#     - append the extracted substring to the 'result' array
+#     - increment the 'num_chars' variable by '1'
+#   - end the inner loop
+#   - increment the 'starting_index' variable by '1'
+# - end the outer loop
+# - return the 'result' array
+
+# is_palindrome? method
+# =====================
+# - Inside the 'is_palindrome?' method, check whther the string
+#   value is equal to its reversed value.  You can use the
+#   String#reverse method.
+
+# palindrome_substrings method
+# ============================
+# - initialize a result variable to an empty array
+# - create an array named substring_arr that contains all of the 
+#   substrings of the input string that are at least 2 characters long
+# - loop through the words in the substring_arr array.
+# - if the word is a palindrome, append it to the result array
+# - return the result array
+
+def substring(str)
+  result = []
+  starting_index = 0;
+
+  while (starting_index <= str.length -2_
+    num_chars = 2
+    while (num_chars <= str.length - starting_index)
+      substring = str.slice(starting_index, num_chars)
+      result << substring
+      num_chars += 1
+    end
+    starting_index += 1
+  end
+  result
+end
+
+def is_palidrom?(str)
+  return str == str.reverse
+end
+
+def palindrome_substrings(str)
+  result = []
+  substrings_arr = substrings(str)
+  substrings_arr.each do |substring|
+    result << susbstring if is_palindrome?(substring)
+  end
+  result
+end
+
+p palindrome_substrings("supercalifragilisticexpialidocious"); # ["ili"]
+p palindrome_substrings("abcddcbA");   # ["bcddcb", "cddc", "dd"]
+p palindrome_substrings("palindrome"); # []
+p palindrome_substrings("");           # []
 
