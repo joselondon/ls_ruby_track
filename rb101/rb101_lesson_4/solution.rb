e838dd7aad5fac5ddb0d3690b1e8d66aee67f98b
joselondon <copovikingj@gmail.com>
2021-06-11 16:40:14 +0100

    Ongoing video transcrpt for PEDAC walkthrough

--- rb101/rb101_lesson_4/solution.rb
+++ rb101/rb101_lesson_4/solution.rb
@@ -1,5 +1,7 @@
 def sum_even_number_row(row_number)
-
+  rows = []
+  # steps 2 & 3
+  rows # TODO: sum the final row and return the sum
 end
 
 def create_row(start_integer, row_length)
@@ -17,7 +19,7 @@ end
 # row number: 2 --> sum of integers in row: 10
 # row number: 4 --> sum of integers in row: 68
 
-# p sum_even_number_row(1) == 2  # true
+p sum_even_number_row(1) # == 2  # true
 # p sum_even_number_row(2) == 10  # true
 # p sum_even_number_row(4) == 68  # true
 
@@ -27,9 +29,9 @@ end
 # start:  4, length: 2 --> [4, 6]
 # start:  8, length: 3 --> [8, 10, 12]
 
-p create_row(2, 1) == [2]         # true
-p create_row(4, 2) == [4, 6]      # true
-p create_row(8, 3) == [8, 10, 12] # true
+# p create_row(2, 1) == [2]         # true
+# p create_row(4, 2) == [4, 6]      # true
+# p create_row(8, 3) == [8, 10, 12] # true
 
 # Create row algorithm:
 # 1.  Create an empty 'row' to contain the integers
@@ -42,3 +44,12 @@ p create_row(8, 3) == [8, 10, 12] # true
 #   - Add the start integer to the row
 #   - Increment the start integer by 2
 #   - Break out of the loop if length of row equals row_length
+
+
+# Main algorithm
+# 1.  Create an empty 'rows' array to contain all of the rows
+# 2.  Create a 'row' array and add it to the overall 'rows' array
+# 3.  Repeat step 2 until all the neccessary rows have been created
+#   - all rows have been created when the length of the 'rows' array is equal to the input integer
+# 4.  Sum the final row
+# 5.  Return the sum
