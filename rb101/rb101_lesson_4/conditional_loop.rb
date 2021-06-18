ac54ca0cf55e86c219310d44220a26d173754376
joselondon <copovikingj@gmail.com>
2021-06-18 11:49:48 +0100

    updates to loopexercises

--- rb101/rb101_lesson_4/conditional_loop.rb
+++ rb101/rb101_lesson_4/conditional_loop.rb
@@ -1,7 +1,8 @@
 process_the_loop = [true, false].sample
 
-if process_the_loop == true
-  p "The loop was processed!"
-else
-  p "The loop wasn't processed!"
-end
+  
+  if process_the_loop == true
+    p "The loop was processed!"
+  else
+    p "The loop wasn't processed!"
+  end
--- rb101/rb101_lesson_4/even_or_odd.rb
+++
@@ -1,7 +0,0 @@
-count = 1
-
-loop do
-  puts count.odd? ? "#{count}: is odd!" : "#{count}: is even!"
-  count += 1
-  break if count == 6
-end
