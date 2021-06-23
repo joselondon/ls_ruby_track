a91a5fe8770ec4cc3b484cf2d0a269190c53e0bf
joselondon <copovikingj@gmail.com>
2021-06-23 20:30:10 +0100

    Adds easy 1 ex 7

--- rb101/rb101_rb109_small_problems/easy_1/stringy_strings.rb
+++ rb101/rb101_rb109_small_problems/easy_1/stringy_strings.rb
@@ -43,3 +43,38 @@ puts stringy(6) == '101010'
 puts stringy(9) == '101010101'
 puts stringy(4) == '1010'
 puts stringy(7) == '1010101'
+
+# modified version rules:
+# 1. allow an  optional argument defaulting to 1.
+# 2. If method is called with this argument set to 0,
+#   the method should return a String of alternating 
+#   0s and 1s but starting with 0 instead of 1.
+# 
+# Algorithm:
+# 1. Allow a second argument, optional and defaulting to 1
+# 2. Check if optional argument is set to 1
+#     if yes 
+#     even indexes are set to 0
+#     else if the optional argument is not supplied or is 0
+#     even indexes are set to 1
+# 3. keep going until string length == integer
+# 4.  return str
+
+def stringy_2(int, srt = 1)
+  str = []
+  int.times do |index|
+    if srt = 1
+      num  = index.even? ? 0 : 1
+    elsif srt = 0
+      num = index.even? ? 1 : 0
+    end
+    str << num
+  end
+  str.join
+end
+
+# tests/examples (always evaluating to true):
+ puts stringy_2(6) == '010101'
+ puts stringy_2(9) == '010101010'
+ puts stringy_2(4) == '0101'
+ puts stringy_2(7) == '0101010'
