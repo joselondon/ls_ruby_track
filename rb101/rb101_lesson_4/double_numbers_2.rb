11c53114d385825feff903c5a266d1adcca6a500
joselondon <copovikingj@gmail.com>
2021-07-04 12:29:13 +0100

    Adds a note to the double_numbers! method code to show original array mutated

--- rb101/rb101_lesson_4/double_numbers_2.rb
+++ rb101/rb101_lesson_4/double_numbers_2.rb
@@ -3,7 +3,7 @@ def double_numbers!(numbers)
 
   loop do
     break if counter == 6
-    
+
     numbers[counter] = numbers[counter] * 2
 
     counter += 1
@@ -14,3 +14,4 @@ end
 
 my_numbers = [1, 4, 3, 7, 2, 6]
 p double_numbers!(my_numbers) # => [2, 8, 6, 14, 4, 12]
+p my_numbers # original array mutated
