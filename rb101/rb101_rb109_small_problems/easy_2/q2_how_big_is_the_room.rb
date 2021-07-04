ec7496834c2154a5b2bf2990053b74c5f0323f5e
joselondon <copovikingj@gmail.com>
2021-07-04 15:46:20 +0100

    Adds start of how big is this room solution (easy 2 q 2

---
+++ rb101/rb101_rb109_small_problems/easy_2/q2_how_big_is_the_room.rb
@@ -0,0 +1,5 @@
+# PROBLEM:
+# Build a program that asks a user for the length and width of a
+# room in meters and then displays the area of the room in both 
+# square meters and square feet. Note: 1 square meter == 10.7639 
+# square feet.
