eaccfa2351ac600a9c1d5adf8caaa382a6902824
joselondon <copovikingj@gmail.com>
2021-07-06 08:29:44 +0100

    Adds solution to tip_calculator.rb

---
+++ rb101/rb101_rb109_small_problems/easy_2/tip_calculator.rb
@@ -0,0 +1,39 @@
+# PROBLEM
+# Create a simple tip calculator. The program should prompt for a 
+# bill amount and a tip rate. The program must compute the tip and
+# then display both the tip and the total amount of the bill.
+
+# ALGORITHM
+# inputs:   float, integer
+# outputs:  string, float
+# 1. run program
+# 2. program prompts user for a bill_amount float
+# 3. (implied) program should accept floats and disregard more than 2 decimal places
+# 4. program should prompt for a tip rate integer
+# 5. compute tip = bill_amount * (tip_rate / 100)
+# 6. compute total = tip + amount
+# 7. Display tip and total to user 
+
+# EXAMPLE:
+# What is the bill? 200
+# What is the tip percentage? 15
+# 
+# The tip is $30.0
+# The total is $230.0
+
+def tip_calculator
+  puts "What is the bill?"
+  bill_amount = gets.to_f.round(2)
+
+  puts "What is the tip percentage?"
+  tip_percentage = gets.to_f / 100
+
+  tip = bill_amount * tip_percentage
+  total = bill_amount + tip
+
+  puts
+  puts "The tip is $#{tip}"
+  puts "The total is $#{total}"
+end
+
+tip_calculator()
