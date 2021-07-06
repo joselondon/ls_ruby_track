35eded6ff3e5c9a0fadbef648fcd886cd960577f
joselondon <copovikingj@gmail.com>
2021-07-06 18:26:27 +0100

    Adds begining - when I reire

---
+++ rb101/rb101_rb109_small_problems/easy_2/when_will_I_retire.rb
@@ -0,0 +1,22 @@
+# PROBLEM:
+# Build a program that displays when the user will retire and how 
+# many years she has to work till retirement.
+
+# EXAMPLE:
+# What is your age? 30
+# At what age would you like to retire? 70
+# 
+# It's 2016. You will retire in 2056.
+# You have only 40 years of work to go!
+
+# ALGORITHM:
+# Inputs: integers (current_age, retirment_age)
+# Outputs: Integer (current_year, years_to_go)
+# 
+# 1. Prompt user for age - store as integer variable 
+# 2. Prompt user for retirement age, store as integer variable
+# 3. Retrieve current year and store as variable
+# 4. Calculate years_to_go = retirement_age - current_age
+# 5. Calculate retirment year = current_year + years_to_go
+# 6. Display message with current_year, retirement_year and and 
+# years_to_go
