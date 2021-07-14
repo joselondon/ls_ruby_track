95b5d110e6899d90a4485f3b29f6a7ecfcd2cc95
joselondon <copovikingj@gmail.com>
2021-07-14 08:28:52 +0100

    Adds answer for practice probem 5

---
+++ rb101/rb101_lesson_4/practice_problems:_methods_and_more_methods.rb/practice_problem_5.rb
@@ -0,0 +1,7 @@
+hash = { a: 'ant', b: 'bear' }
+hash.shift
+
+# => [:a, 'ant']
+
+# check ruby docs https://ruby-doc.org/core-3.0.2/Hash.html#method-i-shift
+# Removes the first hash entry; returns a 2-element Array containing the removed key and value
