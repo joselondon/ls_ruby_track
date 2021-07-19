02704684f6b089b03742cabbdd5c80eb405f441d
joselondon <copovikingj@gmail.com>
2021-07-19 19:10:02 +0100

    TODO  complete quesstion 10

---
+++ rb101/rb101_lesson_4/4.11_practice_problems_addtional_practice/practice_problem_10.rb
@@ -0,0 +1,20 @@
+# Given the munsters hash below
+
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+# Modify the hash such that each member of the Munster family has an 
+# additional "age_group" key that has one of three values describing 
+# the age group the family member is in (kid, adult, or senior). 
+# Your solution should produce the hash below
+# 
+# { "Herman" => { "age" => 32, "gender" => "male", "age_group" => "adult" },
+#   "Lily" => {"age" => 30, "gender" => "female", "age_group" => "adult" },
+#   "Grandpa" => { "age" => 402, "gender" => "male", "age_group" => "senior" },
+#   "Eddie" => { "age" => 10, "gender" => "male", "age_group" => "kid" },
+#   "Marilyn" => { "age" => 23, "gender" => "female", "age_group" => "adult" } }
