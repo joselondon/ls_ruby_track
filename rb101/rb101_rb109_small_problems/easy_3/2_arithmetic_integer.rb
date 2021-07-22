81d9514aecaab4a4a41377d0b179d29a1d99dc31
joselondon <copovikingj@gmail.com>
2021-07-22 19:10:05 +0100

    Adds solution to easy_3.2

--- rb101/rb101_rb109_small_problems/easy_3/2_arithmetic_integer.rb
+++ rb101/rb101_rb109_small_problems/easy_3/2_arithmetic_integer.rb
@@ -15,3 +15,25 @@
 # ==> 23 % 17 = 6
 # ==> 23 ** 17 = 141050039560662968926103
 
+# ALGORITHM
+# 
+# inputs = integers
+# outputs = strings
+# 
+# 1. Get 2 +ve integers
+# 2. Perform each calculation ( +, - , *, /, %, **) using the two inpouted integers
+# 3. Output result as part of a string including the two integers and operator.
+
+
+puts "Enter the first number:"
+int_1 = gets.chomp.to_i
+
+puts "Enter the second number:"
+int_2 = gets.chomp.to_i
+
+puts "#{int_1} + #{int_2} = #{int_1 + int_2} " 
+puts "#{int_1} - #{int_2} = #{int_1 - int_2} " 
+puts "#{int_1} * #{int_2} = #{int_1 * int_2} " 
+puts "#{int_1} / #{int_2} = #{int_1 / int_2} " 
+puts "#{int_1} % #{int_2} = #{int_1 % int_2} " 
+puts "#{int_1} ** #{int_2} = #{int_1 ** int_2} " 
