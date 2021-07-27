b297327ae0b08549797c7eeba136f6e73ab4cdf5
joselondon <copovikingj@gmail.com>
2021-07-27 19:06:08 +0100

    Adds primary solution to easy_3.7

---
+++ rb101/rb101_rb109_small_problems/easy_3/7_odd_lists.rb
@@ -0,0 +1,17 @@
+# Write a method that returns an Array that contains every other element of an Array 
+# that is passed in as an argument. The values in the returned list should be those 
+# values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
+
+def oddities(array)
+  odd_indexed_elements = []
+  array.each_with_index do |e, i|
+    (i == 0 || i.even?) ? odd_indexed_elements << e : next
+  end
+  odd_indexed_elements
+end
+
+puts  oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
+puts  oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
+puts  oddities(['abc', 'def']) == ['abc']
+puts  oddities([123]) == [123]
+puts  oddities([]) == []
