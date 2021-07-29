c5595ec01eb13b93ccbf2d7a3ed94e0740ab977e
joselondon <copovikingj@gmail.com>
2021-07-29 08:34:27 +0100

    Adds solution for easy_3.7

--- rb101/rb101_rb109_small_problems/easy_3/7_odd_lists.rb
+++ rb101/rb101_rb109_small_problems/easy_3/7_odd_lists.rb
@@ -2,30 +2,51 @@
 # that is passed in as an argument. The values in the returned list should be those 
 # values that are in the 1st, 3rd, 5th, and so on elements of the argument Array.
 
-def oddities(array)
+# def oddities(array)
+#   odd_indexed_elements = []
+#   array.each_with_index do |e, i|
+#     (i == 0 || i.even?) ? odd_indexed_elements << e : next
+#   end
+#   odd_indexed_elements
+# end
+
+# def oddities_2 (array)
+#   odd_elements = []
+#   index = 0
+#   while index < array.size
+#     odd_elements << array[index]
+#     index += 2
+#   end
+#   odd_elements
+# end
+require 'pry'
+
+def oddities_3(array)
   odd_indexed_elements = []
-  array.each_with_index do |e, i|
-    (i == 0 || i.even?) ? odd_indexed_elements << e : next
+  length = (array.length) -1
+  for i in 0..length do
+    (i == 0 || i.even?) ? odd_indexed_elements << array[i] : next
   end
   odd_indexed_elements
 end
 
-puts  oddities([2, 3, 4, 5, 6]) == [2, 4, 6]
-puts  oddities([1, 2, 3, 4, 5, 6]) == [1, 3, 5]
-puts  oddities(['abc', 'def']) == ['abc']
-puts  oddities([123]) == [123]
-puts  oddities([]) == []
+p oddities_3([2, 3, 4, 5, 6]) # == [2, 4, 6]
+p oddities_3([1, 2, 3, 4, 5, 6]) #  == [1, 3, 5]
+p oddities_3(['abc', 'def']) #  == ['abc']
+p oddities_3([123]) # == [123]
+p oddities_3([]) #  == []
 
-def evenites(array)
-  even_indexed_elements = []
-  array.each_with_index do |e, i|
-    i.odd? ? even_indexed_elements << e : next
-  end
-  even_indexed_elements
-end
+# def evenites(array)
+#   even_indexed_elements = []
+#   array.each_with_index do |e, i|
+#     i.odd? ? even_indexed_elements << e : next
+#   end
+#   even_indexed_elements
+# end
+# 
+# puts  evenites([2, 3, 4, 5, 6]) == [3, 5]
+# puts  evenites([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
+# puts  evenites(['abc', 'def']) == ['def']
+# puts  evenites([123]) == []
+# puts  evenites([]) == []
 
-puts  evenites([2, 3, 4, 5, 6]) == [3, 5]
-puts  evenites([1, 2, 3, 4, 5, 6]) == [2, 4, 6]
-puts  evenites(['abc', 'def']) == ['def']
-puts  evenites([123]) == []
-puts  evenites([]) == []
