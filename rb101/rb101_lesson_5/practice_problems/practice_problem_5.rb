b24c461a68cb2df9c72642c7abafc58ad0013e45
joselondon <copovikingj@gmail.com>
2021-08-19 14:23:12 +0100

    Practice problems 3 through 7

---
+++ rb101/rb101_lesson_5/practice_problems/practice_problem_5.rb
@@ -0,0 +1,17 @@
+# Given this nested Hash:
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+# figure out the total age of just the male members of the family.
+
+total_male_age = 0
+
+munsters.each do |munster, stats|
+  total_male_age += stats['age'] if stats['gender'] == 'male' 
+end
+
+p total_male_age
