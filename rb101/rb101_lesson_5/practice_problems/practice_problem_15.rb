4e26111b50a4a4d8945688efb9848f387df3ff47
joselondon <copovikingj@gmail.com>
2021-09-03 15:44:39 +0200

    Adds solution for probelm 15

--- rb101/rb101_lesson_5/practice_problems/practice_problem_15.rb
+++ rb101/rb101_lesson_5/practice_problems/practice_problem_15.rb
@@ -3,12 +3,12 @@
 
 arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
 
-new_arr = []
-
-arr.select do |hash|
-  hash.map do |key, array|
-    array.all? do |elem|
-      p elem
+new_arr = arr.select do |hash|
+  hash.all? do |key, array|
+    array.all? do |num|
+      num.even?
     end
   end
 end
+
+p new_arr
