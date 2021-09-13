df49744b238d17fbc85bdd4f7819ff53b203fb9d
joselondon <copovikingj@gmail.com>
2021-09-13 17:44:01 +0100

    Adds display board method

---
+++ rb101/rb101_lesson_6/tic_tac_toe/tictactoe.rb
@@ -0,0 +1,16 @@
+def display_board
+  puts ""
+  puts "     |     |"
+  puts "     |     |"
+  puts "     |     |"
+  puts "-----+-----+-----|"
+  puts "     |     |"
+  puts "     |     |"
+  puts "     |     |"
+  puts "-----+-----+-----|"
+  puts "     |     |"
+  puts "     |     |"
+  puts "     |     |"
+  puts ""
+end
+
