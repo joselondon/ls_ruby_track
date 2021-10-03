9160f173664a5d32c34161637b29c2c8d4168b3b
joselondon <copovikingj@gmail.com>
2021-10-03 13:23:02 +0100

    break_out_of_welcome method name changed to break_out_of_welcome?

--- rb101/rb101_lesson_6/tic_tac_toe/tictactoe.rb
+++ rb101/rb101_lesson_6/tic_tac_toe/tictactoe.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagonals
@@ -61,7 +59,7 @@ def display_board_numbers
   puts "7 | 8 | 9"
 end
 
-def break_out_of_welcome
+def break_out_of_welcome?
   prompt "Hit enter to continue to the game tournament"
   go_to_game = gets.chomp
   !go_to_game.nil?
@@ -240,7 +238,7 @@ end
 
 loop do
   display_welcome_text
-  break_out_of_welcome
+  break_out_of_welcome?
 
   player_score = 0
   computer_score = 0
