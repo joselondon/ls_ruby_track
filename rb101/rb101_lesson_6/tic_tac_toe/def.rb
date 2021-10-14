77920228f53fe46454c59659898312d8fbddaef3
joselondon <copovikingj@gmail.com>
2021-10-14 08:25:57 +0100

    changes

--- rb101/rb101_lesson_6/tic_tac_toe/def.rb
+++
@@ -1,9 +0,0 @@
-brd = {1=>" ", 2=>" ", 3=>" ", 4=>" ", 5=>" ", 6=>" ", 7=>" ", 8=>"X", 9=>"X"}
-
-brd.each do |k, v|
-  if brd[k] == 'X' && brd[k + 1] == 'X'
-    puts 'true'
-  else
-    puts 'false'
-  end
-end
