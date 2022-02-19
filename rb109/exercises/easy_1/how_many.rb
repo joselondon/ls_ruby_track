f7dc178384c600f626fdddc16e773beb3da66b93
joselondon <copovikingj@gmail.com>
2022-02-19 18:49:33 +0000

    Adds revision topis numbers spreadsheet.  Adds easy 1 > how_many solution

--- rb109/exercises/easy_1/how_many.rb
+++ rb109/exercises/easy_1/how_many.rb
@@ -1,3 +1,28 @@
+# Case sensitive
+require 'pry'
 def count_occurrences(arr)
-  
+  labels = arr.uniq
+
+  labels.each do |label|
+    puts "#{label} => #{arr.count(label)}"
+  end
 end
+
+vehicles = [
+  'car', 'car', 'truck', 'car', 'SUV', 'suv', 'truck',
+  'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
+
+# Case insensitive
+def count_occurences_insensitive(arr)
+  arr_downcase = arr.map { |element| element.downcase }
+  labels = arr_downcase.uniq
+
+  labels.each do |label|
+    puts "#{label} => #{arr_downcase.count(label)}"
+  end
+end
+puts
+count_occurences_insensitive(vehicles)
