78b0d2d689b8f4f5a1c673c63023a5004dbcbffa
joselondon <copovikingj@gmail.com>
2022-03-10 17:49:24 +0000

    Adds whats_my_bonus.rb

---
+++ rb109/exercises/easy_1/whats_my_bonus.rb
@@ -0,0 +1,7 @@
+def calculate_bonus(salary, bonus_state)
+  bonus = bonus_state ? salary / 2 : 0
+end
+
+puts calculate_bonus(2800, true) == 1400
+puts calculate_bonus(1000, false) == 0
+puts calculate_bonus(50000, true) == 25000
