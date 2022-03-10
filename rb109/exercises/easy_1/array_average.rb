56ff5db2e647216fe8d23134044d35a71bfc7c8b
joselondon <copovikingj@gmail.com>
2022-03-10 16:58:29 +0000

    Adds array_average.rb

---
+++ rb109/exercises/easy_1/array_average.rb
@@ -0,0 +1,9 @@
+def average(arr)
+  num = 0
+  arr.each_with_object(num) { |element| num += element }
+  num / arr.length
+end
+
+puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
+puts average([1, 5, 87, 45, 8, 8]) == 25
+puts average([9, 47, 23, 95, 16, 52]) == 40
