ca5ae0f37581136ae94efc57dd9f355644ec8741
joselondon <copovikingj@gmail.com>
2022-03-10 17:14:03 +0000

    Adds average_float.rb

--- rb109/exercises/easy_1/array_average.rb
+++ rb109/exercises/easy_1/array_average.rb
@@ -7,3 +7,12 @@ end
 puts average([1, 6]) == 3 # integer division: (1 + 6) / 2 -> 3
 puts average([1, 5, 87, 45, 8, 8]) == 25
 puts average([9, 47, 23, 95, 16, 52]) == 40
+
+
+def average_float(arr)
+  num = 0.0
+  arr.each_with_object(num) { |element| num += element }
+  num / arr.length
+end
+
+puts average_float([1, 6])
