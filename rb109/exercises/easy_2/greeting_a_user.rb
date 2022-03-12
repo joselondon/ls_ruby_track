8623ec7396a1aba56584174fa8ee3fdd7727a2cb
joselondon <copovikingj@gmail.com>
2022-03-12 18:08:57 +0000

    Minor adjustments

--- rb109/exercises/easy_2/greeting_a_user.rb
+++ rb109/exercises/easy_2/greeting_a_user.rb
@@ -1,7 +1,7 @@
 print "What is your name?"
 name = gets.chomp
 if name.end_with?("!")
-  puts "HELLO #{name.upcase}.  WHY ARE WE SCREAMING?"
+  puts "HELLO #{name.chop.upcase}.  WHY ARE WE SCREAMING?"
 else
   puts "Hello #{name}."
 end
