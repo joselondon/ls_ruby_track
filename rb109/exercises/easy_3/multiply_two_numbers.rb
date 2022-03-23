0b115c70c989651e4ca808e2ed5d1a059a5c575f
joselondon <copovikingj@gmail.com>
2022-03-23 18:17:05 +0000

    Adds square method to multiply_two_numbers.rb

--- rb109/exercises/easy_3/multiply_two_numbers.rb
+++ rb109/exercises/easy_3/multiply_two_numbers.rb
@@ -2,6 +2,15 @@ def multiply(num1, num2)
   num1 * num2
 end
 
-puts multiply(3, 5) == 15
+# => multiply(3, 5) == 15
 
-multiply([2, 4], 5) == 
+multiply([2, 4], 5) == [2, 4, 2, 4, 2, 4, 2, 4, 2, 4]
+# It appears the element pattern [2, 4] is muliplies 5 times within 
+# the returned array.
+
+def square(num)
+  multiply(num, num)
+end
+
+p square(5) == 25
+p square(-8) == 64
