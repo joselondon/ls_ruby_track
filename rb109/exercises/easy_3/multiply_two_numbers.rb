22e1bcdeaec494d8fcb91657b66d2afcba493b8c
joselondon <copovikingj@gmail.com>
2022-03-23 19:49:12 +0000

    Adds soltions for suare numbers and power of exerccises withing the multiply_two_numbers.rb file

--- rb109/exercises/easy_3/multiply_two_numbers.rb
+++ rb109/exercises/easy_3/multiply_two_numbers.rb
@@ -14,3 +14,16 @@ end
 
 p square(5) == 25
 p square(-8) == 64
+
+puts 
+
+def power_of(num, n)
+  powered = 1
+  n.times { powered = multiply(powered, num)}
+  powered
+end
+
+p power_of(3, 3) == 27
+p power_of(3, 4) == 81
+p power_of(-4, 8) == 65536
+p power_of(10, 4) == 10000
