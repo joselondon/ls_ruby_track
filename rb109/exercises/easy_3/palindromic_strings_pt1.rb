f37b271f8a0c85653602a023857af0a9660e30c8
joselondon <copovikingj@gmail.com>
2022-03-25 18:34:41 +0000

    Adds palindromic strings further discussions

--- rb109/exercises/easy_3/palindromic_strings_pt1.rb
+++ rb109/exercises/easy_3/palindromic_strings_pt1.rb
@@ -6,3 +6,23 @@ p palindrome?('madam') == true
 p palindrome?('Madam') == false          # (case matters)
 p palindrome?("madam i'm adam") == false # (all characters matter)
 p palindrome?('356653') == true
+
+def palindrome_arr?(array)
+  array.reverse == array
+end
+
+puts
+p palindrome_arr?(['d', 'a', 'd']) == true
+p palindrome_arr?(['D', 'a', 'd']) == false
+
+def palindrome_st_ar?(str_arr)
+  str_arr.reverse == str_arr
+end
+
+puts
+p palindrome_st_ar?('madam') == true
+p palindrome_st_ar?('Madam') == false          # (case matters)
+p palindrome_st_ar?("madam i'm adam") == false # (all characters matter)
+p palindrome_st_ar?('356653') == true
+p palindrome_st_ar?(['d', 'a', 'd']) == true
+p palindrome_st_ar?(['D', 'a', 'd']) == false
