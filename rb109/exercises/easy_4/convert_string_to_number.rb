edf59c332149c31467de6ddc9b0dbb9b867c9b63
joselondon <copovikingj@gmail.com>
2022-04-13 15:45:25 +0100

    Adds solution to string to signed integer problem

--- rb109/exercises/easy_4/convert_string_to_number.rb
+++ rb109/exercises/easy_4/convert_string_to_number.rb
@@ -23,6 +23,30 @@ def hexadecimal_to_integer(str)
   str_arr.reduce {|memo, int| memo*16 + int}
 end
 
+def strip_leading!(str)
+  case str[0]
+  when '-'
+    str.delete('-')
+  when '+'
+    str.delete('+')
+  else
+    str
+  end
+end
+
+def string_to_signed_integer(str)
+  clean_str = strip_leading!(str)
+  int = string_to_integer(clean_str)
+  str[0] == '-' ? -int : int
+end
+
 p string_to_integer('4321')
 p string_to_integer('570')
 p hexadecimal_to_integer('4D9f')
+puts
+puts "Convert string to a Signed Number!"
+puts
+
+p string_to_signed_integer('4321') == 4321
+p string_to_signed_integer('-570') == -570
+p string_to_signed_integer('+100') == 100
