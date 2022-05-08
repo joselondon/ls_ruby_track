8f9600a3de55726e971f2ff0f0e92135f8842742
joselondon <copovikingj@gmail.com>
2022-05-08 11:08:44 +0100

    Adds solutions that fixes time format to accomodate left and right padding with '0's.  Solution now covers greater 0 - 1439, > 1439, 0, and < -1439.   TODO -1 - -1439

--- rb109/exercises/easy_5/after_midnight_part_1.rb
+++ rb109/exercises/easy_5/after_midnight_part_1.rb
@@ -1,20 +1,20 @@
 def one_and1439(int)
   hrs = sprintf("%02d", (int/2).divmod(30)[0])
-  mins = sprintf("%-2d", int%60)
+  mins = sprintf("%002d", int%60)
   "#{hrs}:#{mins}"
 end
 
 def grtr_than1439(int)
   int = int%1440
     hrs = sprintf("%02d", (int/2).divmod(30)[0])
-  mins = sprintf("%d0", int%60)
+  mins = sprintf("%002d", int%60)
   "#{hrs}:#{mins}"
 end
 
 def time_of_day(int)
   if int > 0 && int <= 1439
     one_and1439(int)
-  elsif int > 1439
+  elsif int > 1439 || int < 0
     grtr_than1439(int)
   else
     "00:00"
@@ -24,10 +24,10 @@ end
 
 # 720 mins in 12 hours
 
-p time_of_day(0) == "00:00"
+p time_of_day(0) # == "00:00"
 # time_of_day(-3) == "23:57"
-p time_of_day(35) == "00:35"
+p time_of_day(35) # == "00:35"
 # time_of_day(-1437) == "00:03"
-p time_of_day(3000) == "02:00"
-p time_of_day(800)  == "13:20"
-p time_of_day(-4231) == "01:29"
+p time_of_day(3000) # == "02:00"
+p time_of_day(800)  # == "13:20"
+p time_of_day(-4231) # == "01:29"
