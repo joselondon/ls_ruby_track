a420767eceb200d1081b03df8880df67ff8f636d
joselondon <copovikingj@gmail.com>
2022-05-15 16:13:05 +0100

    Starts letter_swap.rb file

---
+++ rb109/exercises/easy_5/letter_swap.rb
@@ -0,0 +1,8 @@
+=begin 
+Given a string of words separated by spaces, write a method that takes this string of 
+words and returns a string in which the first and last letters of every word are swapped.
+
+You may assume that every word contains at least one letter, and that the string will always 
+contain at least one word. You may also assume that each string contains nothing but words and spaces
+=end
+
