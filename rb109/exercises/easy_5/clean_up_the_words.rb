e1da35e114433117d069221ccd338738cc60109b
joselondon <copovikingj@gmail.com>
2022-06-04 12:59:34 +0100

    Refactors to use #squeeze String method to de-deupe contiguous ' ' chars.

--- rb109/exercises/easy_5/clean_up_the_words.rb
+++ rb109/exercises/easy_5/clean_up_the_words.rb
@@ -4,13 +4,14 @@ characters, write a method that returns that string with all of the non-alphabet
 replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have 
 one space in the result (the result should never have consecutive spaces).
 =end
-ALPHA_NUM = ('a'..'z').to_a << ' '
+ALPHA = ('a'..'z').to_a << ' '
 
 def cleanup(string)
   new_string = string.chars.select do |elem|
-    ALPHA_NUM.include?(elem)
+    ALPHA.include?(elem)
   end
-  new_string.join.gsub(/\s+/, ' ')
+  new_string.join.squeeze(' ')
+  # new_string.join.gsub(/\s+/, ' ')
 end
 
 p cleanup("---what's my +*& line?") # == ' what s my line ' 
