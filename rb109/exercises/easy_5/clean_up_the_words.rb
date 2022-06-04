3ba997184a1c0e468ccfbd41f33b402e18ad43ee
joselondon <copovikingj@gmail.com>
2022-06-04 12:42:48 +0100

    Fixes issue with double space

--- rb109/exercises/easy_5/clean_up_the_words.rb
+++ rb109/exercises/easy_5/clean_up_the_words.rb
@@ -4,15 +4,13 @@ characters, write a method that returns that string with all of the non-alphabet
 replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have 
 one space in the result (the result should never have consecutive spaces).
 =end
-require 'pry'
-
 ALPHA_NUM = ('a'..'z').to_a << ' '
 
 def cleanup(string)
   new_string = string.chars.select do |elem|
     ALPHA_NUM.include?(elem)
   end
-  new_string.join
+  new_string.join.gsub(/\s+/, ' ')
 end
 
 p cleanup("---what's my +*& line?") # == ' what s my line ' 
